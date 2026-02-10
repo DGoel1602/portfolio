8108beb88336438141ea1b4858e83adf44f1d7ab
DGoel1602 <rhygonfn@gmail.com>
2026-02-10 15:13:51 -0500

    fix: refactor nav bar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -3,15 +3,26 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function NavBar() {
-  const pathName = usePathname();
-  const current =
-    pathName == "/"
-      ? "About"
-      : pathName == "/projects"
-        ? "Projects"
-        : "Experience";
+function NavItem(props: { route: string, name: string }) {
+	const current = usePathname();
+	return (
+        <Link href={props.route} className="content-center">
+          <h4
+            className={`text-2xl ${current == props.route ? "" : "hover:"}text-white font-bold relative group cursor-pointer`}
+          >
+					{ props.name }
+            <span
+              className={`absolute bottom-0 left-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == props.route ? "" : "hover:"}w-1/2`}
+            />
+            <span
+              className={`absolute bottom-0 right-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == props.route ? "" : "hover:"}w-1/2`}
+            />
+          </h4>
+        </Link>
+	)
+}
 
+export default function NavBar() {
   return (
     <div className="z-1 flex justify-around px-3 min-w-screen bg-none h-[60pt] absolute top-0">
       <div className="text-white h-full content-center w-1/4">
@@ -20,45 +31,9 @@ export default function NavBar() {
         </Link>
       </div>
       <div className="flex justify-around h-full content-center w-1/3">
-        <Link href="/" className="content-center">
-          <h4
-            className={`text-2xl ${current == "About" ? "" : "hover:"}text-white font-bold relative group cursor-pointer`}
-          >
-            About
-            <span
-              className={`absolute bottom-0 left-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == "About" ? "" : "group-hover:"}w-1/2`}
-            />
-            <span
-              className={`absolute bottom-0 right-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == "About" ? "" : "group-hover:"}w-1/2`}
-            />
-          </h4>
-        </Link>
-        <Link href="/projects" className="content-center">
-          <h4
-            className={`text-2xl ${current == "Projects" ? "" : "hover:"}text-white font-bold relative group cursor-pointer`}
-          >
-            Projects
-            <span
-              className={`absolute bottom-0 left-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == "Projects" ? "" : "group-hover:"}w-1/2`}
-            />
-            <span
-              className={`absolute bottom-0 right-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == "Projects" ? "" : "group-hover:"}w-1/2`}
-            />
-          </h4>
-        </Link>
-        <Link href="/experience" className="content-center">
-          <h4
-            className={`text-2xl ${current == "Experience" ? "" : "hover:"}text-white font-bold relative group cursor-pointer`}
-          >
-            Experience
-            <span
-              className={`absolute bottom-0 left-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == "Experience" ? "" : "group-hover:"}w-1/2`}
-            />
-            <span
-              className={`absolute bottom-0 right-1/2 h-[2px] w-0 bg-current transition-all duration-300 ${current == "Experience" ? "" : "group-hover:"}w-1/2`}
-            />
-          </h4>
-        </Link>
+				<NavItem route="/" name="About" />
+				<NavItem route="/projects" name="Projects" />
+				<NavItem route="/experience" name="Experience" />
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,9 @@ export default function Home() {
         height="2000"
         className="min-w-screen min-h-screen"
       />
-      <div className="absolute inset-0 bg-[#1D122A] opacity-40 mix-blend-multiply" />
+      <div className="absolute inset-0 bg-[#1D122A] opacity-50 mix-blend-multiply" />
+			<h1 className="text-5xl min-w-screen text-center absolute top-[40%]"> &quot;Some random ass quote&quot; </h1>
+			<p className="text-3xl min-w-screen text-center absolute top-[47%]"> - Some guy probably </p>
     </>
   );
 }
