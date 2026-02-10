858633d96ffb6967631dac6c2e55dae1c2eec679
DGoel1602 <rhygonfn@gmail.com>
2026-02-10 14:21:02 -0500

    fix: cool gif instead of bg

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
     <>
       <Image
         alt="Background image"
-        src="/cool-bg.jpg"
+        src="/cool-bg.gif"
         width="2000"
         height="2000"
         className="min-w-screen min-h-screen"
       />
-      <div className="absolute inset-0 bg-[#1D122A] opacity-50 mix-blend-multiply" />
+      <div className="absolute inset-0 bg-[#1D122A] opacity-40 mix-blend-multiply" />
     </>
   );
 }
