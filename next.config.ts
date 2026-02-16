d95aa530185db63c055d9729f65b7209be387fea
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2026-02-16 14:14:24 -0500

    Add redirect from '/gh' to GitHub profile

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/gh',
+        destination: 'https://github.com/DGoel1602',
+        permanent: true,
+      }
+    ];
+  }
 };
 
 export default nextConfig;
